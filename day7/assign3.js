3a2464ebfd75af0fce1ce82437de49946fffd8c4
“payalsh14” <payal7703shekhawat@gmail.com>
2022-08-10 14:13:49 +0530

    hm

--- day7/assign3.js
+++ day7/assign3.js
@@ -3,14 +3,26 @@ const person =
     firstName:"", 
     Age:"", 
     Address:"", 
+    City:"",
+    State:"",
+    Pincode: "",
     Hobbies:"",
+
     pr: function() {
-        return "Name :"+ this.firstName + "\t" + "Age :" + this.Age+ "\t" + "Address :" +this.Address+ "\t" + "Hobbies :"+this.Hobbies;
+        return "Name :"+ this.firstName + "\t" + "Age :" + this.Age+ "\t" + "Address :" +this.Address+ "\t" + "Hobbies :"+this.Hobbies +"City :" +this.City+"State :" +this.State + "Pincode :" +this.Pincode;
     }
 
     };
 person.firstName=prompt("Enter Name :");
 person.Age=prompt("Enter Age");
 person.Address=prompt("Enter Address :");
+person.City=prompt("Enter City :");
+person.State=prompt("Enter State :");
+person.Pincode=Number(prompt("Enter Pincode :"));
+
 person.Hobbies=prompt("Enter Hobbies:");
+document.body.style.backgroundColor="black";
+document.body.style.color="aqua";
+document.body.style.fontSize="40px";
+document.body.style.fontFamily="cursive";
 document.write('<h1 style="text-align:center; color:black;">'+person.pr()+'</h1>')
