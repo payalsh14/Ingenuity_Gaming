847c810a5e6169245104b9502e57afa9595d1906
“payalsh14” <payal7703shekhawat@gmail.com>
2022-08-10 14:24:23 +0530

    hm

--- day7/assign3.js
+++ day7/assign3.js
@@ -5,25 +5,18 @@ const person =
     Address:"", 
     City:"",
     State:"",
-    Pincode: "",
+    Pincode:"",
     Hobbies:"",
-
     pr: function() {
-        return "Name :"+ this.firstName + "\t" + "Age :" + this.Age+ "\t" + "Address :" +this.Address+ "\t" + "Hobbies :"+this.Hobbies +"City :" +this.City+"State :" +this.State + "Pincode :" +this.Pincode;
+        return "Name : "+ this.firstName + "\n" + "<br><br>Age : " + this.Age+ "\n" + "<br><br>Address : " +this.Address+ "\n" + "<br><br>City : "+this.City+ "\n" + "<br><br>State : " +this.State+ "\n" + "<br><br>Pincode : "+this.Pincode+ "\n" + "<br><br>Hobbies : "+this.Hobbies; 
     }
 
     };
-    document.body.style.backgroundColor="black";
 person.firstName=prompt("Enter Name :");
 person.Age=prompt("Enter Age");
 person.Address=prompt("Enter Address :");
 person.City=prompt("Enter City :");
 person.State=prompt("Enter State :");
-person.Pincode=Number(prompt("Enter Pincode :"));
-
+person.Pincode=prompt("Enter Pincode :");
 person.Hobbies=prompt("Enter Hobbies:");
-document.body.style.backgroundColor="black";
-document.body.style.color="aqua";
-document.body.style.fontSize="40px";
-document.body.style.fontFamily="cursive";
-document.write('<h1 style="text-align:center; color:black;">'+person.pr()+'</h1>')
+document.write('<h1 style="text-align:center; color:chocolate;">'+person.pr()+'</h1>')
